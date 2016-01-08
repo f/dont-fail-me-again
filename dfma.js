f204696d621687a20be007100bb1de266b30dff1
Phạm Ngọc Quang Nam <nampnq@gmail.com>
2016-01-08 18:24:32 +0700

    Update dfma.js
    Support linux, and...other platform

--- dfma.js
+++ dfma.js
@@ -1,5 +1,12 @@
 process.on('uncaughtException', function (err) {
-  console.error(err.stack)
-  require('child_process').exec('curl -s http://play.starwars.com/starwars_soundboard/asset/audio/DV_darth_vader/vader_20.mp3 > /tmp/dfma.mp3 && afplay /tmp/dfma.mp3')
-  process.exit(1)
+  var 
+    players = {
+      'darwin': 'afplay',
+      'linux': 'mpg123'
+    },
+    player = players.indexOf(process.platform) >= 0 ?? players[process.platform] : 'none';
+  
+  console.error(err.stack);
+  require('child_process').exec('curl -s http://play.starwars.com/starwars_soundboard/asset/audio/DV_darth_vader/vader_20.mp3 > /tmp/dfma.mp3 && ' + player + '  /tmp/dfma.mp3');
+  process.exit(1);
 });
